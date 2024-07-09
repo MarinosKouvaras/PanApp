0433e5c111e15c84aa48fbd00edaa02520866bd2
MarinosKouvaras <ap23011@hua.gr>
2024-07-09 21:26:36 +0300

    dockerize

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+require('dotenv').config()
+var createError = require('http-errors')
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
@@ -14,8 +14,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+    // 404 Not Found.
+    if (err.status === 404) {
+      return res
+        .status(404)
+    }
+  
+    // 500 Internal Server Error (in production, all other errors send this response).
+    if (req.app.get('env') !== 'development') {
+      return res
+        .status(500)
+    }
+  
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+  
+    // render the error page
+    res.status(err.status || 500)
+})
+
+// Start listening.
+app.listen(3000, async () => {
+    console.log('Server started on http://localhost:3000')
+    console.log('Press Ctrl-C to terminate...')
+})
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/re', function(req, res, next) {
+  res.render('indexe', { title: 'Express' });
 });
 
 module.exports = router;
